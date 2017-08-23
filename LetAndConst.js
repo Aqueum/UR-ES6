49e3e19ed521673f498dcbd64664779613968268
Aqueum <martin@aqueum.com>
2017-08-23 17:02:46 +0100

    Initial commit

---
+++ LetAndConst.js
@@ -0,0 +1,21 @@
+function getClothing(isCold) {
+    if (isCold) {
+        var freezing = 'Grab a jacket!';
+    } else {
+        var hot = 'It’s a shorts kind of day.';
+        console.log(freezing);
+    }
+}
+
+getClothing(false)
+
+function getClothing2(isCold) {
+    if (isCold) {
+        const freezing = 'Grab a jacket!';
+    } else {
+        const hot = 'It’s a shorts kind of day.';
+        console.log(freezing);
+    }
+}
+
+getClothing2(false)
