77b82c53132a782919917e0562b08870b0adafbb
Aqueum <martin@aqueum.com>
2017-08-24 23:48:21 +0100

    Undefine

--- LetAndConst.js
+++ LetAndConst.js
@@ -1,21 +1,21 @@
 function getClothing(isCold) {
-    if (isCold) {
-        var freezing = 'Grab a jacket!';
-    } else {
-        var hot = 'It’s a shorts kind of day.';
-        console.log(freezing);
-    }
+  if (isCold) {
+    var freezing = 'Grab a jacket!';
+  } else {
+    var hot = 'It’s a shorts kind of day.';
+    console.log(freezing);
+  }
 }
 
-getClothing(false)
+getClothing(false);
 
-function getClothing2(isCold) {
-    if (isCold) {
-        const freezing = 'Grab a jacket!';
-    } else {
-        const hot = 'It’s a shorts kind of day.';
-        console.log(freezing);
-    }
-}
+// function getClothing2(isCold) {
+//   if (isCold) {
+//     const freezing = 'Grab a jacket!';
+//   } else {
+//     const hot = 'It’s a shorts kind of day.';
+//     console.log(freezing);
+//   }
+// }
 
-getClothing2(false)
+// getClothing2(false);
